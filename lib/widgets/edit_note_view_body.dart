0e571581da0aa01a3b665cb5d849e6af9a0fc15c
Omar <omarbro224@gmail.com>
2025-09-24 14:58:57 +0300

    finish edit note view ui

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_text_form_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -18,6 +19,19 @@ class EditNoteViewBody extends StatelessWidget {
           CustomAppBar(
             text: "Edit Note",
             icon: Icons.check,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          CustomTextFormField(
+            hintText: 'Title',
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          CustomTextFormField(
+            hintText: 'Content',
+            maxLines: 5,
           ),
         ],
       ),
