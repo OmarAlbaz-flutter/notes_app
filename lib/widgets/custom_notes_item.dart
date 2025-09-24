2ce07c39d7a100275bf507225be2b3c21af0d2d2
Omar <omarbro224@gmail.com>
2025-09-24 14:48:34 +0300

    edit note view

--- lib/widgets/custom_notes_item.dart
+++ lib/widgets/custom_notes_item.dart
@@ -1,64 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class CustomNotesItem extends StatelessWidget {
   const CustomNotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-        left: 16,
-        top: 24,
-        bottom: 24,
-      ),
-      margin: EdgeInsets.symmetric(vertical: 6),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.orange.shade200,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              "Flutter Tips",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 32,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                "Build your career with Omar Albaz",
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(
+          EditNoteView.id,
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(
+          left: 16,
+          top: 24,
+          bottom: 24,
+        ),
+        margin: EdgeInsets.symmetric(vertical: 6),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.orange.shade200,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                "Flutter Tips",
                 style: TextStyle(
-                  color: Colors.black.withAlpha(150),
-                  fontSize: 18,
+                  color: Colors.black,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  "Build your career with Omar Albaz",
+                  style: TextStyle(
+                    color: Colors.black.withAlpha(150),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 32,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 32,
+            Padding(
+              padding: const EdgeInsets.only(right: 24, top: 16),
+              child: Text(
+                "May 22 2025",
+                style: TextStyle(
+                  color: Colors.black.withAlpha(150),
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24, top: 16),
-            child: Text(
-              "May 22 2025",
-              style: TextStyle(
-                color: Colors.black.withAlpha(150),
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
