5893fb3990072cc7162a5f64675aee509fdd388a
Omar <omarbro224@gmail.com>
2025-09-27 18:49:53 +0300

    add color pick to note bottom sheet done

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
@@ -7,11 +8,10 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
+  Color color = Color(0xFFDDD8B8);
   addNote(NoteModel note) async {
-    emit(
-      AddNoteLoading(),
-    );
+    note.color = color.toARGB32();
+    emit(AddNoteLoading());
 
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
